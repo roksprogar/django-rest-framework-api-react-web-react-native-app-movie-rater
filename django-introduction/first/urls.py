e5e738574c5983b696735032d1b1ae2bf14d5ece
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 11:05:12 +0100

    add basic auth token endpoint

--- django-introduction/first/urls.py
+++ django-introduction/first/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', obtain_auth_token),
 ]
