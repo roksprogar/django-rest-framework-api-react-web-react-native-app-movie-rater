b885ae178ec620b10f09397afecd8e6ccf98108d
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 09:31:56 +0100

    add a basic template

--- django-introduction/demo/urls.py
+++ django-introduction/demo/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from demo import views
-from demo.views import Another
 
 urlpatterns = [
     path('first', views.first),
-    path('another', Another.as_view()),
 ]
--- django-introduction/demo/views.py
+++ django-introduction/demo/views.py
@@ -1,18 +1,5 @@
-from django.http import HttpResponse
-from django.views import View
-from demo.models import Book
-
-
-class Another(View):
-    books = Book.objects.filter(is_published=True)
-    output = ''
-
-    for book in books:
-        output += f"We have {book.title} books with ID {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
+from django.shortcuts import render
 
 
 def first(request):
-    return HttpResponse('First message from views.')
+    return render(request, 'first_temp.html')
