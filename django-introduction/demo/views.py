92fc0e47ee8a072c6439b466367ddaea933102cb
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 11:25:07 +0100

    add auth and permission classes

--- django-introduction/demo/views.py
+++ django-introduction/demo/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from demo.models import Book
 from demo.serializers import BookSerializer
@@ -7,3 +9,5 @@ from demo.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
