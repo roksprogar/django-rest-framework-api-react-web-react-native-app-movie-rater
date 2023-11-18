3b5afdf48f4ff83cafc452783c8d28e47de3ac83
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 09:11:08 +0100

    add books db fetching

--- django-introduction/demo/views.py
+++ django-introduction/demo/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.views import View
+from demo.models import Book
 
 
 class Another(View):
+    books = Book.objects.filter(is_published=True)
+    output = ''
+
+    for book in books:
+        output += f"We have {book.title} books with ID {book.id}<br>"
+
     def get(self, request):
-        return HttpResponse('Another message from views.')
+        return HttpResponse(self.output)
 
 
 def first(request):
