c97695b487b0e37bd9a043026dd5c7d12c940730
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 17:48:35 +0100

    add movie and rating models

--- MovieRaterAPI/api/admin.py
+++ MovieRaterAPI/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api.models import Movie, Rating
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    pass
