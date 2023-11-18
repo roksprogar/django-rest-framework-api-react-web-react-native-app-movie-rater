d36f6fffefcd3a77a68d913c22c89aef489d1d3a
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-18 08:41:57 +0100

    add class-based view

--- django-introduction/demo/urls.py
+++ django-introduction/demo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from demo import views
+from demo.views import Another
 
 urlpatterns = [
     path('first', views.first),
+    path('another', Another.as_view()),
 ]
--- django-introduction/demo/views.py
+++ django-introduction/demo/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.views import View
+
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('Another message from views.')
 
 
 def first(request):
