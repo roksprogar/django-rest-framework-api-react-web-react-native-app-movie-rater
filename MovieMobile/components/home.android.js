d8a5176102286f897a0f3cde22eb6fc8e8728f1e
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-19 10:59:28 +0100

    platform specific code

---
+++ MovieMobile/components/home.android.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
+export default function Home({msg}) {
+    const [name, setName] = useState('Rok')
+  return (
+    <View style={styles.home}>
+        <Text>I'm on Android!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  home: {
+    flex: 1,
+    backgroundColor: Platform.OS == 'ios' ? '#fff': '#aaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+  },
+
+});
