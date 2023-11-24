1f6ec2a85d5bc3c9f18eadb0c76a6ee98cf3b884
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-24 21:22:24 +0100

    add movie list component

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState(['Movie 1', 'Movie 2'])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie rater</h1>
       </header>
       <div className='layout'>
-        <div>Movie list</div>
+        <div>
+          { movies.map( movie => {
+            return <h2>{movie}</h2>
+          })}
+        </div>
         <div>Movie details</div>
       </div>
     </div>
