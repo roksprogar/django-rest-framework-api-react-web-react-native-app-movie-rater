f83260b5c86016172aa7347a04c47ecf54511b43
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-24 22:02:10 +0100

    add movies fetch useffect

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -1,8 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2'])
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('http://0.0.0.0:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 42c3f93e53684418e372619c503ace234a56685f',
+      }
+    })
+    .then(response => response.json())
+    .then(response => setMovies(response))
+    .catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
