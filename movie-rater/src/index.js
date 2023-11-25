886199f5236edf647b35d0a41e45880c3dff60ce
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-25 17:50:44 +0100

    add routing

--- movie-rater/src/index.js
+++ movie-rater/src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App'
+import Auth from './components/auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />,
+  },
+  {
+    path: "/movies",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
