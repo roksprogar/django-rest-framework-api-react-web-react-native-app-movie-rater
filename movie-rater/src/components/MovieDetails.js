a3ed80fbd750b4c2bc13ae5a9014bf4af5afac12
Rok Sprogar <rok.sprogar@gmail.com>
2023-11-25 09:49:40 +0100

    add fontawsome icons

--- movie-rater/src/components/MovieDetails.js
+++ movie-rater/src/components/MovieDetails.js
@@ -1,3 +1,6 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 function MovieDetails(props) {
   return (
     <div>
@@ -5,6 +8,7 @@ function MovieDetails(props) {
         <>
             <h1>{props.movie.title}</h1>
             <p>{props.movie.description}</p>
+            <FontAwesomeIcon icon={faStar} />
         </>
     ): null}
     </div>
